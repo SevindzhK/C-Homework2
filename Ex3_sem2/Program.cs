8a19783cd1bbd799636ec1ab9a5657325df9c677
SevindzhK <sevinge0709@gmail.com>
2022-09-05 10:44:08 +0300

    izmenila resheniye zadachi

--- Ex3_sem2/Program.cs
+++ Ex3_sem2/Program.cs
@@ -5,14 +5,22 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 void weekends(int num)
 {
-    if (num == 6 || num == 7)
-    {
-       Console.WriteLine("Yippee! It's a weekend :)"); 
-    }
-    else 
-    {
-       Console.WriteLine("Oh no! It's a working day :("); 
-    }
+   if (num > 0 && num < 8)
+   {
+      if (num == 6 || num == 7)
+      {
+         Console.WriteLine("Yippee! It's a weekend :)"); 
+      }
+      else 
+      {
+         Console.WriteLine("Oh no! It's a working day :("); 
+      }
+   }
+   else 
+   {
+      Console.WriteLine("You entered an invalid number. Enter a number from 1 to 7"); 
+   }
 }
+   
 weekends(number);
 
